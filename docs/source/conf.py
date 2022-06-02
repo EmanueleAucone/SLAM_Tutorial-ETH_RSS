aab16e5177a97adf3deb202309f58d6f9acd4bf3
eaucone <47002282+EmanueleAucone@users.noreply.github.com>
2022-06-02 16:33:35 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SLAM Tutorial - ETH  RSS'
+copyright = '2022, Emanuele Aucone'
+author = 'eaucone'
 
 release = '0.1'
 version = '0.1.0'
